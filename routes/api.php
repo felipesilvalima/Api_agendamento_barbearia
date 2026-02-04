863e3048477a552aa097348ae17036cc400c2f81
Felipe silva lima <felipesilvalima200@gmail.com>
2026-02-04 17:02:56 -0300

    versionamento da api

--- routes/api.php
+++ routes/api.php
@@ -25,56 +25,61 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//rota de autenticação e criação de usuário de clientes
-Route::post('/login',[AuthController::class, 'login'])->name('logar_usuario');
-Route::post('/clientes',[ClienteController::class, 'criarClientes'])->name('criar_cliente');
+Route::prefix('v1')->group(function (){
 
-
-//rotas de auth
-Route::middleware('auth:api','permissao:Cliente|Barbeiro')->prefix('auth')->group( function () {
-    Route::post('/refresh',[AuthController::class, 'refresh'])->name('refresh');
-    Route::get('/me',[AuthController::class, 'me'])->name('me');
-    Route::patch('/users/password',[AuthController::class, 'uptdateMe'])->name('atualizar_password');
-    Route::delete('/users/desativar',[AuthController::class, 'desativarMe'])->name('desativar_conta');
-    Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+    //rota de autenticação e criação de usuário de clientes
+    Route::post('/login',[AuthController::class, 'login'])->name('logar_usuario');
+    Route::post('/clientes',[ClienteController::class, 'criarClientes'])->name('criar_cliente');
+    
+    
+    //rotas de auth
+    Route::middleware('auth:api','permissao:Cliente|Barbeiro')->prefix('auth')->group( function () {
+        Route::post('/refresh',[AuthController::class, 'refresh'])->name('refresh');
+        Route::get('/me',[AuthController::class, 'me'])->name('me');
+        Route::patch('/users/password',[AuthController::class, 'uptdateMe'])->name('atualizar_password');
+        Route::delete('/users/desativar',[AuthController::class, 'desativarMe'])->name('desativar_conta');
+        Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+    });
+    
+    
+    //rotas acessada por Clientes
+    Route::middleware('auth:api','permissao:Cliente')->group( function () {
+        Route::get('/clientes/agendamentos',[ClienteController::class, 'listarAgendamentosClientes'])->name('listar_agendamentos_clientes');
+        Route::post('/agendamentos',[AgendamentoController::class, 'criarAgendamento'])->name('criar_agendamentos');
+        Route::patch('/agendamentos/{id}/reagendar',[AgendamentoController::class, 'reagendarAgendamentos'])->name('reagendar_agendamentos');
+        Route::delete('/agendamentos/{id_agendamento}/servicos/{id_servico}',[AgendamentoController::class, 'removerServicos'])->name('remover_servicos');
+        Route::post('/agendamentos/{id_agendamento}/servicos/{id_servico}',[AgendamentoServicoController::class, 'adicionarServicosAgendamento'])->name('adiconar_servicos_no_agendamento');
+        Route::patch('/clientes',[ClienteController::class, 'atualizarClientes'])->name('atualizar_clientes');
+    });
+    
+    //rotas Acessada por Barbeiros
+    Route::middleware('auth:api','permissao:Barbeiro')->group( function () {
+        Route::patch('/agendamentos/{id}/concluir',[AgendamentoController::class, 'finalizarAgendamentos'])->name('finalizar_agendamentos');
+        Route::post('/barbeiros',[BarbeiroController::class, 'criarBarbeiros'])->name('criar_barbeiros');
+        Route::get('/barbeiros/agendamentos',[BarbeiroController::class, 'listarAgendamentosBarbeiros'])->name('listar_agendamentos_barbeiros');
+        Route::patch('/barbeiros',[BarbeiroController::class, 'atualizarBarbeiros'])->name('atualizar_barbeiros');
+        Route::post('/servicos',[ServicoController::class, 'criarServicos'])->name('cadastrar_servicos');
+        Route::patch('/servicos/{id_servico}',[ServicoController::class, 'atualizarServicos'])->name('atualizar_servicos');
+        Route::delete('/servicos/{id_servico}/desativar',[ServicoController::class, 'DesativarServicos'])->name('desativar_servicos');
+    });
+    
+    //rotas Acessadas por Cliente e Barbeiro
+    Route::middleware('auth:api','permissao:Cliente|Barbeiro')->group( function () {
+        Route::get('/agendamentos',[AgendamentoController::class, 'listarAgendamentos'])->name('listar_agendamentos');
+        Route::get('/agendamentos/{id}',[AgendamentoController::class, 'verAgenda'])->name('detalhes_agendamentos');
+        Route::get('/agendamentos/{id_agendamento}/servicos',[AgendamentoServicoController::class, 'listaServicosAgendamento'])->name('listar_servicos_por_agendamento');
+        Route::patch('/agendamentos/{id}/cancelar',[AgendamentoController::class, 'cancelarAgendamentos'])->name('cancelar_agendamentos');
+        Route::get('/barbeiros/{id_barbeiro}',[BarbeiroController::class, 'detalhesBarbeiros'])->name('detalhes_barbeiros');
+        Route::get('/clientes/{id_cliente}',[ClienteController::class, 'detalhesClientes'])->name('detalhes_clientes');
+        Route::get('/servicos',[ServicoController::class, 'listarServicos'])->name('listar_servicos');
+        Route::get('/agendamentos/{id_agendamento}/total',[ServicoController::class, 'precoTotalAgendamento'])->name('precoTotal_por_agendamento');
+        Route::get('/servicos/{id_servico}',[ServicoController::class, 'detalhesServicos'])->name('detalhes_servico');
+        Route::get('/notificacoes',[NotificacaoController::class, 'listarNotificacoes'])->name('lista_notificacoes');
+        Route::delete('/notificacoes/{id_notificao}',[NotificacaoController::class, 'deletarNotificaos'])->name('deleta_notificacoes');
+    });
 });
 
 
-//rotas acessada por Clientes
-Route::middleware('auth:api','permissao:Cliente')->group( function () {
-    Route::get('/clientes/agendamentos',[ClienteController::class, 'listarAgendamentosClientes'])->name('listar_agendamentos_clientes');
-    Route::post('/agendamentos',[AgendamentoController::class, 'criarAgendamento'])->name('criar_agendamentos');
-    Route::patch('/agendamentos/{id}/reagendar',[AgendamentoController::class, 'reagendarAgendamentos'])->name('reagendar_agendamentos');
-    Route::delete('/agendamentos/{id_agendamento}/servicos/{id_servico}',[AgendamentoController::class, 'removerServicos'])->name('remover_servicos');
-    Route::post('/agendamentos/{id_agendamento}/servicos/{id_servico}',[AgendamentoServicoController::class, 'adicionarServicosAgendamento'])->name('adiconar_servicos_no_agendamento');
-    Route::patch('/clientes',[ClienteController::class, 'atualizarClientes'])->name('atualizar_clientes');
-});
-
-//rotas Acessada por Barbeiros
-Route::middleware('auth:api','permissao:Barbeiro')->group( function () {
-    Route::patch('/agendamentos/{id}/concluir',[AgendamentoController::class, 'finalizarAgendamentos'])->name('finalizar_agendamentos');
-    Route::post('/barbeiros',[BarbeiroController::class, 'criarBarbeiros'])->name('criar_barbeiros');
-    Route::get('/barbeiros/agendamentos',[BarbeiroController::class, 'listarAgendamentosBarbeiros'])->name('listar_agendamentos_barbeiros');
-    Route::patch('/barbeiros',[BarbeiroController::class, 'atualizarBarbeiros'])->name('atualizar_barbeiros');
-    Route::post('/servicos',[ServicoController::class, 'criarServicos'])->name('cadastrar_servicos');
-    Route::patch('/servicos/{id_servico}',[ServicoController::class, 'atualizarServicos'])->name('atualizar_servicos');
-    Route::delete('/servicos/{id_servico}/desativar',[ServicoController::class, 'DesativarServicos'])->name('desativar_servicos');
-});
-
-//rotas Acessadas por Cliente e Barbeiro
-Route::middleware('auth:api','permissao:Cliente|Barbeiro')->group( function () {
-    Route::get('/agendamentos',[AgendamentoController::class, 'listarAgendamentos'])->name('listar_agendamentos');
-    Route::get('/agendamentos/{id}',[AgendamentoController::class, 'verAgenda'])->name('detalhes_agendamentos');
-    Route::get('/agendamentos/{id_agendamento}/servicos',[AgendamentoServicoController::class, 'listaServicosAgendamento'])->name('listar_servicos_por_agendamento');
-    Route::patch('/agendamentos/{id}/cancelar',[AgendamentoController::class, 'cancelarAgendamentos'])->name('cancelar_agendamentos');
-    Route::get('/barbeiros/{id_barbeiro}',[BarbeiroController::class, 'detalhesBarbeiros'])->name('detalhes_barbeiros');
-    Route::get('/clientes/{id_cliente}',[ClienteController::class, 'detalhesClientes'])->name('detalhes_clientes');
-    Route::get('/servicos',[ServicoController::class, 'listarServicos'])->name('listar_servicos');
-    Route::get('/agendamentos/{id_agendamento}/total',[ServicoController::class, 'precoTotalAgendamento'])->name('precoTotal_por_agendamento');
-    Route::get('/servicos/{id_servico}',[ServicoController::class, 'detalhesServicos'])->name('detalhes_servico');
-    Route::get('/notificacoes',[NotificacaoController::class, 'listarNotificacoes'])->name('lista_notificacoes');
-    Route::delete('/notificacoes/{id_notificao}',[NotificacaoController::class, 'deletarNotificaos'])->name('deleta_notificacoes');
-});
 
 
 
