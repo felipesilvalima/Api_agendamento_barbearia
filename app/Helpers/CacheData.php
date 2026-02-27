7eeb652aa8b079e5b1f9842b219e04a3d625419c
felipesilvalima <felipesilvalima@gmail.com>
2026-02-27 11:39:21 -0300

    alterações atual

--- app/Helpers/CacheData.php
+++ app/Helpers/CacheData.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Cache;
 
 trait CacheData{
 
-public function verificarCache(string $cacheKey)
+public function verificarCache(string $cacheKey):mixed
 {
     if(Cache::has($cacheKey))
     {
@@ -13,7 +13,7 @@ public function verificarCache(string $cacheKey)
     }
 }
 
-public function adicionarCache(string $cacheKey, mixed $value, int $duracao)
+public function adicionarCache(string $cacheKey, mixed $value, int $duracao):void
 {
     Cache::put($cacheKey, $value, now()::addMinuto($duracao));
 }
